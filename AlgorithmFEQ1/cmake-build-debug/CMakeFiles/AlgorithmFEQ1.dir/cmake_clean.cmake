26879dad80aaecbf58ba7b92d0e0990f21c2f8a7
0424jerry <71858802+0424jerry@users.noreply.github.com>
2021-06-08 11:08:39 +0800

    Add files via upload

---
+++ AlgorithmFEQ1/cmake-build-debug/CMakeFiles/AlgorithmFEQ1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AlgorithmFEQ1.exe"
+  "AlgorithmFEQ1.exe.manifest"
+  "AlgorithmFEQ1.pdb"
+  "CMakeFiles/AlgorithmFEQ1.dir/main.cpp.obj"
+  "libAlgorithmFEQ1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgorithmFEQ1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
